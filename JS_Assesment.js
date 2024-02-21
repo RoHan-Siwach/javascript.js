ead4949809196aac3b3e3f719eaec782eb8877ce
RoHan-Siwach <108146993+RoHan-Siwach@users.noreply.github.com>
2024-02-21 22:13:18 +0530

    Update JS_Assesment.js

--- JS_Assesment.js
+++ JS_Assesment.js
@@ -20,15 +20,10 @@ function mintNFT (_name, _eyecolor, _shirtType, _bling) {
     "eye_color": _eyecolor,
     "shirtType": _shirtType,
     "bling": _bling
-
     }
     NFTs.push(NFT);
     console.log("Minted: "+_name);
-
-
   }
-
-
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
@@ -36,16 +31,15 @@ function listNFTs () {
   for(let i=0;i<NFTs.length;i++){
     console.log("\nID: "+(i+1));
     console.log("Name: "+NFTs[i].name);
-    console.log("eyecolor: "+NFTs[i].shirtType);
+    console.log("Eye Color: "+NFTs[i].eye_color);
+    console.log("Shirt Type: "+NFTs[i].shirtType);
     console.log("Bling: "+NFTs[i].bling);
   }
-
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
   console.log("\n"+NFTs.length);
-
 }
 
 // call your functions below this line
